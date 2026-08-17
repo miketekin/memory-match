cfc2005d4ad039739c61f6c58c0ba2df73db21a4
miketekin <10396229+miketekin@users.noreply.github.com>
2026-08-17 05:50:54 +0000

    Finished placing components and assigning images to squares

--- memory-match/src/App.tsx
+++ memory-match/src/App.tsx
@@ -1,61 +1,106 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
+
+function randomize(catIds) {
+  //console.log(catIds)
+  let arrayCopy = catIds
+  for (let i = 11; i >= 0; i--) {
+    //console.log([i, arrayCopy])
+    let tmp = arrayCopy[i]
+    let randomInt = getRandomInt(i)
+    arrayCopy[i] = arrayCopy[randomInt]
+    arrayCopy[randomInt] = tmp
+  }
+  //console.log(arrayCopy)
+  return arrayCopy;
+}
+
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+
 function controlBoard() {
-  function onStartClick() {
-    return;
+  /* how does a timer work?
+    when the timers state is true, it increments by 1 every second
+    how do we increment by 1 every second?
+    We could capture the start time then have the timer evaluate the current time
+    minus the start time and rerender that component every time it changes
+    how do we capture the start time?
+    Date.now() captures epoch time including ms
+    divide by 1000 to get seconds
+    
+    while the start state is true, run date.now() every second and subtract from the first capture?
+    If we're already running something every second, then we could just increment using that
+
+  */ 
+  const [activity, setActivity] = useState("Start");
+  let buttonClass = "start-button"
+  const [timer, setTimer] = useState(false);
+  const time = 0;
+  function onActivityClick() {
+    if (activity == "Start") {
+      setActivity("Stop")
+      setTimer(true)
+    }
+    else {
+      setActivity("Start")
+      setTimer(false)
+    }
   }
 
-  function onEndClick() {
-    return;
+  if (activity == "Start") {
+    buttonClass = "board-element start-button"
+  }
+  else {
+    buttonClass = "board-element stop-button"
   }
   
   return(
     <div className="control-board">
-        <div className="board-element">Start</div>
-        <div className="board-element">End</div>
-        <div className="board-element timer">0.0</div>
+        <div className={buttonClass} onClick={onActivityClick}>{activity}</div>
+        <div className="board-element timer">Time: 0.0</div>
+        <div className="board-element timer">Score: 0.0</div>
     </div>
   )
 }
 
-
-function gameBoard() {
-  /*
-    each square needs to return an image and
-    that image needs to be click
-  */
-  function Square({image}) {
+function gameBoard({swappedIds}) {
+  
+  function Square({catId}) {
+    const basePath = "https://cataas.com/cat/"
+    const params = "?type=square&position=center"
+    const fullPath = basePath+catId+params
     return (
       <img 
-      src={image}
+      src={fullPath}
       className="square"
       onClick={() => console.log("clicked")}
-      >
-
-      </img>
+      ></img>
     )
   }
- 
+
   return (
     <div className="game-board">
       <div className="board-row">
-        <Square image="https://robohash.org/241.156.51.13.png"/>
-        <Square image="https://robohash.org/241.156.51.13.png"/>
-        <Square image="https://robohash.org/241.156.51.13.png"/>
-        <Square image="https://robohash.org/241.156.51.13.png"/>
+        <Square catId = {swappedIds[0]}/>
+        <Square catId = {swappedIds[1]}/>
+        <Square catId = {swappedIds[2]}/>
+        <Square catId = {swappedIds[3]}/>
       </div>
       <div className="board-row">
-        <Square image="https://robohash.org/241.156.51.13.png"/>
-        <Square image="https://robohash.org/241.156.51.13.png"/>
-        <Square image="https://robohash.org/241.156.51.13.png"/>
-        <Square image="https://robohash.org/241.156.51.13.png"/>
+        <Square catId = {swappedIds[4]}/>
+        <Square catId = {swappedIds[5]}/>
+        <Square catId = {swappedIds[6]}/>
+        <Square catId = {swappedIds[7]}/>
       </div>
       <div className="board-row">
-        <Square image="https://robohash.org/241.156.51.13.png"/>
-        <Square image="https://robohash.org/241.156.51.13.png"/>
-        <Square image="https://robohash.org/241.156.51.13.png"/>
-        <Square image="https://robohash.org/241.156.51.13.png"/>
+        <Square catId = {swappedIds[8]}/>
+        <Square catId = {swappedIds[9]}/>
+        <Square catId = {swappedIds[10]}/>
+        <Square catId = {swappedIds[11]}/>
       </div>
     </div>
   )
@@ -63,15 +108,19 @@ function gameBoard() {
 
 
 function App() {
-  const [catIds, setCatIds] = useState(null);
-
-  useEffect(() => {
-    fetch("https://cataas.com/api/cats?limit=6")
+  const [swappedIds, setSwappedIds] = useState("X");
+  const skipMax: number = 1975;
+  const skip: number = getRandomInt(skipMax)
+    useEffect(() => {
+    fetch("https://cataas.com/api/cats?limit=6&skip="+skip)
     .then((result) => result.json())
-    .then((json) => setCatIds(json))
+    .then((json) => json.map(({id}) => id))
+    .then((catIds) => catIds.concat(catIds))
+    .then((catIds) => setSwappedIds(randomize(catIds)))
     }, []
   );
 
+  return(
   <div className="App">
     <header className="App-header">
       <p>
@@ -87,11 +136,12 @@ function App() {
         {controlBoard()}
       </div>
       <div className="game-board">
-        {gameBoard()}
+        {gameBoard({swappedIds})}
       </div>
     </div>
 
   </div>
+  )
 }
 
 
